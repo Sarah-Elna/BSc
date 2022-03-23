c4f813d7b91e4bae7d23f50a7aa7b8467d10b473
Sarah-Elna <elna.elisa@gmail.com>
2022-03-23 12:28:23 +0100

    update

--- HybPiper_test.py
+++ HybPiper_test.py
@@ -16,10 +16,10 @@ gwf = Workflow()
 ########################################################################################################################
 ################################################---- Hybpiper ----######################################################
 ########################################################################################################################
-def hybpiper(species, p1, p2, un, path_out, path_in, done):
+def hybpiper(species, paired_1, paired_2, unpaired, path_out, path_in, done_file):
     """Hybpiper."""
     inputs = [path_in + species + paired_1, path_in + species + paired_2, path_in + species + unpaired] # The files which the job will look for before it runs
-    outputs = [path_out + species, done] # The files which will have to be created in order for the job to be "completed"
+    outputs = [path_out + species, done_file] # The files which will have to be created in order for the job to be "completed"
     options = {'cores': 1, 'memory': "20g", 'walltime': "100:00:00"} #Slurm commands
 
     spec = """
@@ -30,7 +30,7 @@ def hybpiper(species, p1, p2, un, path_out, path_in, done):
 /home/sarahe/HybPiper/reads_first.py --cpu 16 -r {p1} {p2} --unpaired {un} -b /home/sarahe/GitHub/BSc/matK_rbcL_psbA_target.fasta --prefix {sp} --bwa
 
     touch {done}
-    """.format(species=species, p1 = path_in + species + paired_1, p2 = path_in + species + paired_2, un = path_in + species + unpaired , out = path_out, done = done)
+    """.format(sp=species, p1 = path_in + species + paired_1, p2 = path_in + species + paired_2, un = path_in + species + unpaired , out = path_out, done = done)
 
 
     return (inputs, outputs, options, spec)
@@ -50,9 +50,11 @@ sp = ['0001', '0002', '0003', '0004', '0005', '0006', '0007', '0008', '0009', '0
 
 for i in range(len(sp)):
     gwf.target_from_template('Hybpiper_'+sp[i], hybpiper(species = sp[i],
-                                                        p1 = "_clean-READ1.fastq",
-                                                        p2 = "_clean-READ2.fastq",
-                                                        un = "_clean-READ12-single.fastq",
-                                                        path_out= "/home/sarahe/BSc/01_HybPiper/",
+                                                        paired_1 = "_clean-READ1.fastq",
+                                                        paired_2 = "_clean-READ2.fastq",
+                                                        unpaired = "_clean-READ12-single.fastq",
+                                                        path_out = "/home/sarahe/BSc/01_HybPiper/",
                                                         path_in = "/home/sarahe/BSc/00_data/",
-                                                        done = "/home/sarahe/BSc/01_HybPiper/done/Hybpiper/"+sp[i]))
+                                                        done_file = "/home/sarahe/BSc/01_HybPiper/done/Hybpiper/"+species[i]))
+
+# species, paired_1, paired_2, unpaired, path_out, path_in, done_file
