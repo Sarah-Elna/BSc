6941ded096c2bfdcffba26c154273d703cc8247e
Sarah-Elna <elna.elisa@gmail.com>
2022-03-30 14:34:41 +0200

    get names fixed

--- Python_Scripts/get_data_names.py
+++ Python_Scripts/get_data_names.py
@@ -5,7 +5,7 @@ from os import listdir
 from os.path import isfile, join
 
 # set path to directory
-my_path1 = os.path.abspath("C:/Users/Sarah/Documents/AU/6/Bachelor/GenomeDK/Filer_fra_wolf/trimmed/")
+my_path1 = os.path.abspath("C:/Users/Sarah/Documents/AU/6/Bachelor/GenomeDK/Filer_fra_wolf/trimmed_kopi/")
 
 # set path to names.txt
 my_path2 = os.path.abspath("C:/Users/Sarah/Documents/AU/6/Bachelor/GitHub/BSc")
@@ -14,11 +14,12 @@ my_path2 = os.path.abspath("C:/Users/Sarah/Documents/AU/6/Bachelor/GitHub/BSc")
 file_names = [f for f in listdir(my_path1) if isfile(join(my_path1, f))]
 
 # get the first four unique cifers of the files
-first_four_list=[]
-for four in file_names:
-    first_four = four[0:4]
-    if first_four not in first_four_list:
-        first_four_list.append(first_four)
+name_list=[]
+for file_name in file_names:
+    if file_name.endswith("12-single.fastq"):
+        name = file_name[0:-26]
+        if name not in name_list:
+            name_list.append(name)
 
 # create the empty names.txt file
 n = open('{}\\names.txt'.format(my_path2), 'w')
@@ -26,7 +27,7 @@ n.write("")
 n.close()
 
 # adds the unique four cifers to the names.txt file
-for i in range(len(first_four_list)):
+for i in range(len(name_list)):
     n = open('{}\\names.txt'.format(my_path2), 'a')
-    n.write("{}\n".format(first_four_list[i]))
+    n.write("{}\n".format(name_list[i]))
     n.close()
