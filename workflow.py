d8d1349cf245dd7f020a8f3a3c862e274e742141
Sarah-Elna <elna.elisa@gmail.com>
2022-03-31 13:09:59 +0200

    jgbhbm

--- workflow.py
+++ workflow.py
@@ -46,13 +46,12 @@ def read_csv(file_name, file_delimiter):
     with open(file_name) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=file_delimiter)
         for row in csv_reader:
-            for i in range(0, len(ending)):
-                file_path = ("/home/sarahe/BSc/00_data/"+str(row[1])+"_clean-Read1.fastq")
-                isFile = os.path.isfile(file_path)
-                if (isFile == True) and row[1] not in name_list:
-                    name_list.append(row[1])
-                else:
-                    print('could not find' + file_path)
+            file_path = ("/home/sarahe/BSc/00_data/"+str(row[1])+"_clean-Read1.fastq")
+            isFile = os.path.isfile(file_path)
+            if (isFile == True) and row[1] not in name_list:
+                name_list.append(row[1])
+            else:
+                print('could not find' + file_path)
     return name_list
 
 rename = "/home/sarahe/GitHub/BSc/Renaming_csv_files/Rename_Files.csv"
