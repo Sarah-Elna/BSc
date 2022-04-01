3a4c83ec4e9cca9822562085b58f615cf21472fe
Sarah-Elna <elna.elisa@gmail.com>
2022-04-01 12:41:33 +0200

    try fix problem with done files not being created

--- workflow.py
+++ workflow.py
@@ -30,11 +30,10 @@ def hybpiper(species, p1, p2, un, path_out, path_in, done):
 
     cd {out}
         
-    /home/sarahe/HybPiper/reads_first.py --cpu 16 --readfiles {p1} {p2} --unpaired {un} -b /home/sarahe/GitHub/BSc/Target_filer/Renamed_Target_file3.fasta --prefix {species} --bwa
+    /home/sarahe/HybPiper/reads_first.py --cpu 16 --readfiles {p1} {p2} --unpaired {un} -b /home/sarahe/GitHub/BSc/Target_filer/Wolf_Target.fasta --prefix {species} --bwa
     touch {done}
     """.format(species=species, p1 = path_in + species + p1, p2 = path_in + species + p2, un = path_in + species + un , out = path_out, done = done)
-
-
+    
     return (inputs, outputs, options, spec)
 
 ########################################################################################################################
@@ -65,6 +64,6 @@ for i in range(0, 10):
                                                         p1 = "_clean-Read1.fastq",
                                                         p2 = "_clean-Read2.fastq",
                                                         un = "_clean-Read12-single.fastq",
-                                                        path_out = "/home/sarahe/BSc/01_HybPiper/",
+                                                        path_out = "/home/sarahe/BSc/01_HybPiper_wolf_test/",
                                                         path_in = "/home/sarahe/BSc/00_data/",
-                                                        done = "/home/sarahe/BSc/01_HybPiper/done/Hybpiper/"+sp[i]+'.txt'))
+                                                        done = "/home/sarahe/BSc/01_HybPiper_wolf_test/done/Hybpiper/"+sp[i]))
