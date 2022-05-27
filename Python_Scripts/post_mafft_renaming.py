a1bb059cf48bb847413c2a172955278090135069
Sarah-Elna <elna.elisa@gmail.com>
2022-05-27 12:42:47 +0200

    avaoid duplicats

--- Python_Scripts/post_mafft_renaming.py
+++ Python_Scripts/post_mafft_renaming.py
@@ -5,12 +5,19 @@ parser.add_argument("in_file")
 args = parser.parse_args()
 in_file = str(args.in_file)
 
+duplicats_startswith_0 = ['DHET0', 'DHET1', 'MRDA0', 'MRDA1', 'MRDA2', 'MRDA3', 'MSKO0', 'MSKO1', 'DLEU0', 'DLEU1', 'MRIN0', 'MRIN1', 'MRIN2']
+duplicats_other = ['DLAN1', 'DLAN2', 'DLAN3']
+
 def rename_targets(in_file_path):
     file = open(in_file, 'r')
     file_line_list = file.readlines()
     for line in file_line_list:
         if line != '' and line[0] == '>':
             new_line = line[0:6]
+            if duplicats_startswith_0 in new_line:
+                new_line = line[0:5]+'0'
+            if duplicats_other in new_line:
+                new_line = line[0:5]+'1'
             new_line.strip()
             print(new_line, end='\n')
         else:
